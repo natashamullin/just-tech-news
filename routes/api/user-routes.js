a773b1ecf5718f89c1b8e11073456d2d8e162872
Natasha Mullin <69641107+natashamullin@users.noreply.github.com>
2020-12-16 17:44:18 -0800

    password protected

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -47,6 +47,28 @@ router.post('/', (req, res) => {
         });
 });
 
+router.post('/login', (req, res) => {
+    //expets {email: '...', paswword: '...'}
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+        }
+        // res.json({ user: dbUserData });
+        //Verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.ststus(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+});
+
 router.put('/:id', (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
